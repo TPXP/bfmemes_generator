90188b567ab7066bcf3e0926229b96a704af2b53
Thomas P <tpxp@live.fr>
2020-09-05 23:00:41 +0200

    WIP: Enhance the form to edit an element

--- src/component/ElementForm.vue
+++ src/component/ElementForm.vue
@@ -1,20 +1,22 @@
 <template>
-  <el-form>
-    <el-form-item label="Color" v-if="element.color">
-      <el-color-picker :value="element.color" @input="setValue('color', $event, true)" />
-    </el-form-item>
-  </el-form>
+  <div class="elementForm" v-if="element">
+    <div class="option inline" v-if="['color', 'text'].includes(element.type)">
+      <div class="label">Couleur</div>
+
+    </div>
+    <div class="option inline"> <!-- v-if="['color', 'text'].includes(element.type)"> -->
+      <div class="label">Texte</div>
+      <input placeholder="texte" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "ElementForm",
-  props: {
-    elementIndex: Number,
-  },
   computed: {
     element(){
-      return this.$store.state.elements[this.elementIndex];
+      return this.$store.state.elements[this.$store.state.selectedElement] ?? {};
     }
   },
   methods:{
@@ -31,5 +33,35 @@ export default {
 </script>
 
 <style scoped>
-
+.elementForm{
+  padding:10px 10px 0;
+}
+.option{
+  padding-bottom: 10px;
+  font-size: 17px;
+}
+.option.inline{
+  flex-direction: row;
+  height:50px;
+  display: flex;
+  align-items: center;
+}
+.option .label{
+  font-weight: bold;
+  width:120px;
+}
+input {
+  background: #fff0;
+  border: 0 none;
+  border-bottom:1px solid #bbb;
+  height:40px;
+  padding:5px;
+  color: white;
+  flex:1;
+  transition: all 300ms;
+}
+input:focus{
+  background: #fff2;
+  border-bottom-color: #fff;
+}
 </style>
